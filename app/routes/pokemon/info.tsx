96f0709772eb9c9554c534cd85bf02132f0d56c1
shehabnagy2020 <shehab.nagy2020@gmail.com>
2025-11-23 20:00:36 +0200

    clean up

--- app/routes/pokemon/info.tsx
+++ app/routes/pokemon/info.tsx
@@ -1,8 +1,16 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { AiFillThunderbolt, AiOutlineArrowLeft } from "react-icons/ai";
 import { useParams } from "react-router";
+import { useGetPokemonInfo } from "~/api/hooks/useGetPokemonInfo";
+import AbilityState from "~/components/abilityState";
+import PokemonInfoCard from "~/components/pokemonInfoCard";
+import PokemonInfoCardSkeleton from "~/components/pokemonInfoCard/skeleton";
+import StatsProgress from "~/components/statsProgress";
+import TypesBadges from "~/components/typesBadges";
 
 const Info = () => {
   const { pokemonId } = useParams();
+  const { data, isLoading, error, refetch } = useGetPokemonInfo(pokemonId);
 
   useEffect(() => {
     // if id is wrong redirect to 404
@@ -11,7 +19,42 @@ const Info = () => {
     }
   }, [pokemonId]);
 
-  return <div className="">Pokemon {pokemonId}</div>;
+  return (
+    <div className="bg-[#faedf7] min-h-[100vh]">
+      <div className="container xl:w-2/4 mx-auto py-8">
+        <div className="mb-4 flex px-4 md:px-0">
+          <a
+            href="/"
+            className="bg-white cursor-pointer p-2 rounded-lg flex items-center gap-2 border-2 border-gray-200"
+          >
+            <AiOutlineArrowLeft />
+            Back to list
+          </a>
+        </div>
+
+        {!isLoading ? (
+          data && !error ? (
+            <PokemonInfoCard card={data} />
+          ) : (
+            <div
+              data-testid="error-message"
+              className="text-red-500 text-center w-full col-span-full"
+            >
+              Error loading Pokemon info,{" "}
+              <button
+                onClick={() => refetch()}
+                className="cursor-pointer underline"
+              >
+                Retry
+              </button>
+            </div>
+          )
+        ) : (
+          <PokemonInfoCardSkeleton />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Info;
